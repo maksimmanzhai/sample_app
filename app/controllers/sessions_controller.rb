6ddc77e85944fbf1d281c4c77b5df87091c197df
maksimmanzhai <bender23max@gmail.com>
2019-03-24 15:14:58 +0300

    Log in with remember function

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,5 @@
 class SessionsController < ApplicationController
+  
   def new
   end
 
@@ -6,6 +7,7 @@ class SessionsController < ApplicationController
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
       log_in user
+      remember user
       redirect_to user
     else
       flash.now[:danger] = 'Invalid email/password combination'
@@ -17,4 +19,5 @@ class SessionsController < ApplicationController
     log_out
     redirect_to root_url
   end
+
 end

c985f511e4366cc4c2b353cc7e3950ce893c70cd
maksimmanzhai <bender23max@gmail.com>
2019-04-02 13:55:23 +0300

    Add activation account of user and reset password

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'password_resets/new'
+  get 'password_resets/edit'
   get 'sessions/new'
   get 'users/new'
   root             'static_pages#home'
@@ -8,6 +10,7 @@ Rails.application.routes.draw do
   get 'signup'  => 'users#new'
   resources :users
   resources :account_activations, only: [:edit]
+  resources :password_resets, only: [:new, :create, :edit, :update]
 
   get 'login'   => 'sessions#new'
   post 'login'  => 'sessions#create'

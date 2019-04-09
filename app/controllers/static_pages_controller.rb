587065034d3aa3db5ef942fdfec596c51f0624b5
maksimmanzhai <bender23max@gmail.com>
2019-04-09 12:34:14 +0300

    Add variable @micropost to method home

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,5 +1,7 @@
 class StaticPagesController < ApplicationController
+
   def home
+    @micropost = current_user.microposts.build if logged_in?
   end
 
   def help
@@ -8,6 +10,7 @@ class StaticPagesController < ApplicationController
   def about
   end
 
-  def contact    
+  def contact
   end
+
 end

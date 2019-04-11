6130dbff71dda2f3a065f41c3ebf6f86331db733
maksimmanzhai <bender23max@gmail.com>
2019-04-11 18:59:41 +0300

    Adding the routes for user relationships.

--- config/routes.rb
+++ config/routes.rb
@@ -16,7 +16,7 @@ Rails.application.routes.draw do
   resources :account_activations, only: [:edit]
   resources :password_resets, only: [:new, :create, :edit, :update]
   resources :microposts, only: [:create, :destroy]
-  
+  resources :relationships,       only: [:create, :destroy]
 
   get 'login'   => 'sessions#new'
   post 'login'  => 'sessions#create'
